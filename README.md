dfeb6ae7aa60d37c795ccb4ba89a10bb58cb0463
Keith Bingman <keith@mobify.com>
2013-09-30 09:59:24 -0700

    updated license

--- README.md
+++ README.md
@@ -1,1 +1,87 @@
-Pikabu!
+# Pikabu
+
+## A speedy, flexible framework for off-canvas flyout panels.
+
+<em>Simple markup</em> &mdash; Few containers and classes means
+implementation is a breeze.
+
+<em>Native scrolling</em> &mdash; Just like mom used to make. Momentum
+scrolling in sidebars and smart fallbacks for browsers that don&rsquo;t support it.
+
+<em>Customization</em> &mdash; No theme, just barebones HTML and CSS.
+Pikabu is super customizable to your needs.
+
+<em>Compatibility</em> &mdash; This thing works on everything.
+We progressively enhance the experience for devices that support it.
+
+
+## Usage
+
+    $(el).pikabu(options);
+
+## Methods
+
+## Classes
+
+## Events
+
+## Browser Compatibility
+
+### Mobile Browsers
+
+The following mobile browsers are fully supported:
+
+| Browser           | Version |
+|-------------------|---------|
+| Mobile Safari     | 3.1.3+  |
+| Android Browser   | 2.1+    |
+| Android Chrome    | 1.0+    |
+| Android Firefox   | 1.0+    |
+
+The following mobile browsers have degraded support:
+
+| Browser           | Version |
+|-------------------|---------|
+| Windows Phone     | 7.5     |
+
+### Desktop Browsers
+
+The follow desktop browsers are fully supported:
+
+| Browser           | Version |
+|-------------------|---------|
+| Safari            | 4.0+    |
+| Firefox           | 4.0+    |
+| Chrome            | 12.0+   |
+| Opera             | 12.0+   |
+| Internet Explorer | 10.0+   |
+
+The following desktop browsers have degraded support:
+
+| Browser           | Version |
+|-------------------|---------|
+| Internet Explorer | 8.0,9.0 |
+| Firefox           | 3.5,3.6 |
+
+## Building
+### Requirements
+* [node.js 0.8.x/npm](http://nodejs.org/download/)
+
+### Steps
+1. `npm install -g grunt-cli`
+2. `npm install`
+3. `grunt`
+
+The build directory will be populated with minified versions of the css and 
+javascript files and a .zip of the original source files (for distribution and
+use with whatever build system you might use).
+
+## Contributing
+
+Please see the project's GitHub page for details contributing
+
+## License
+
+Copyright 2013 Mobify Research and Development Inc.
+
+Licensed under the MIT License
--- tests/main.js
+++ tests/main.js
@@ -1,108 +1,108 @@
 (function($) {
 
-	// Markers for events
-	var initMarker = false, openedMarker = false, closedMarker = false;
-	// Set up settings for Pikabu object specified in tests/index.html
-	var settings = {
-		viewportSelector: '.test-viewport',
-		selectors: {
-			element: '.test-container',
-			common: '.test-sidebar',
-			left: '.test-sidebar-left',
-			right: '.test-sidebar-right',
-			navToggles: '.nav-toggle'
-		},
-		onInit: function() {
-			initMarker = true;
-		},
-		onOpened: function() {
-			openedMarker = true;
-		},
-		onClosed: function() {
-			closedMarker = true;
-		}
-	};
-	var pikabuTest = new Pikabu(settings);
+    // Markers for events
+    var initMarker = false, openedMarker = false, closedMarker = false;
+    // Set up settings for Pikabu object specified in tests/index.html
+    var settings = {
+        viewportSelector: '.test-viewport',
+        selectors: {
+            element: '.test-container',
+            common: '.test-sidebar',
+            left: '.test-sidebar-left',
+            right: '.test-sidebar-right',
+            navToggles: '.nav-toggle'
+        },
+        onInit: function() {
+            initMarker = true;
+        },
+        onOpened: function() {
+            openedMarker = true;
+        },
+        onClosed: function() {
+            closedMarker = true;
+        }
+    };
+    var pikabuTest = new Pikabu(settings);
 
-	// Test fixture variables
-	var windowWidth = $(window).width();
-	var windowHeight = $(window).height();
-	var documentHeight = $(document).height();
-	var responsiveBreakpoint = 768; // (in px) We always show sidebar beyond this breakpoint
-	// Error messages for sidebar visibility tests according to screensize
-	var visibilityErrors = {
-	       true: "Pikabu sidebars aren't visible on a wide screen",
-	       false: "Pikabu sidebars aren't visible on a narrow screen"
-	};
-	var isWidescreen = windowWidth > responsiveBreakpoint;
+    // Test fixture variables
+    var windowWidth = $(window).width();
+    var windowHeight = $(window).height();
+    var documentHeight = $(document).height();
+    var responsiveBreakpoint = 768; // (in px) We always show sidebar beyond this breakpoint
+    // Error messages for sidebar visibility tests according to screensize
+    var visibilityErrors = {
+           true: "Pikabu sidebars aren't visible on a wide screen",
+           false: "Pikabu sidebars aren't visible on a narrow screen"
+    };
+    var isWidescreen = windowWidth > responsiveBreakpoint;
 
-	// Object creation tests
-	test("Pikabu initialization tests", function() {
-		// Object created successfully
-		equal(true, pikabuTest instanceof Pikabu,  "Pikabu not initialized");
-	});
+    // Object creation tests
+    test('Pikabu initialization tests', function() {
+        // Object created successfully
+        equal(true, pikabuTest instanceof Pikabu,  'Pikabu not initialized');
+    });
 
-	test("Pikabu sidebars visibility on initial state", function() {
-		// TODO Find a way to simulate screen widths
-		equal(pikabuTest.$sidebars['left'].is(':visible') && 
-			pikabuTest.$sidebars['right'].is(':visible'), isWidescreen, 
-			visibilityErrors[isWidescreen]);
-	});
+    test('Pikabu sidebars visibility on initial state', function() {
+        // TODO Find a way to simulate screen widths
+        equal(pikabuTest.$sidebars['left'].is(':visible') && 
+            pikabuTest.$sidebars['right'].is(':visible'), isWidescreen, 
+            visibilityErrors[isWidescreen]);
+    });
 
-	test("Pikabu sidebars always shown when JS is disabled", function() {
-		
-		// Simulate no JS to always show sidebars
-		$('html').addClass('no-js');
+    test('Pikabu sidebars always shown when JS is disabled', function() {
+        
+        // Simulate no JS to always show sidebars
+        $('html').addClass('no-js');
 
-		equal(pikabuTest.$sidebars['left'].is(':visible') && 
-			pikabuTest.$sidebars['right'].is(':visible'), true, 
-			"Pikabu sidebars aren't visible with JS disabled");
+        equal(pikabuTest.$sidebars['left'].is(':visible') && 
+            pikabuTest.$sidebars['right'].is(':visible'), true, 
+            "Pikabu sidebars aren't visible with JS disabled");
 
-		$('html').removeClass('no-js');
-	});
+        $('html').removeClass('no-js');
+    });
 
-	test("Pikabu sidebar opens on clicking toggle", function() {
+    test('Pikabu sidebar opens on clicking toggle', function() {
 
-		// Only test if the sidebars are hidden by default
-		if(!isWidescreen) {
+        // Only test if the sidebars are hidden by default
+        if(!isWidescreen) {
 
-			// Test nav toggles
-			pikabuTest.$navToggles.each(function(index, el) {
-				
-				var $el = $(el);
-				var role = $el.data('role');
+            // Test nav toggles
+            pikabuTest.$navToggles.each(function(index, el) {
+                
+                var $el = $(el);
+                var role = $el.data('role');
 
-				stop(); // Stop test until async operation completes
-				$el.trigger('click'); // Open sidebar
+                stop(); // Stop test until async operation completes
+                $el.trigger('click'); // Open sidebar
 
-				// Verify that sidebar opens
-				setTimeout(function() {
-					var visible = pikabuTest.$sidebars[role].is(':visible');
-					equal(visible, true, role + " sidebar not opening on clicking toggle");
-					start(); // Resume tests
-				});
+                // Verify that sidebar opens
+                setTimeout(function() {
+                    var visible = pikabuTest.$sidebars[role].is(':visible');
+                    equal(visible, true, role + " sidebar not opening on     clicking toggle");
+                    start(); // Resume tests
+                });
 
-				// Verify that sidebar closes
-				stop();
-				pikabuTest.$overlay.trigger('click'); // Close sidebar
-				setTimeout(function() {
-					var visible = pikabuTest.$sidebars[role].is(':visible');
-					equal(visible, false, role + " sidebar visible after clicking on overlay");
-					start(); // Resume tests
-				}, 500);
+                // Verify that sidebar closes
+                stop();
+                pikabuTest.$overlay.trigger('click'); // Close sidebar
+                setTimeout(function() {
+                    var visible = pikabuTest.$sidebars[role].is(':visible');
+                    equal(visible, false, role + " sidebar visible after clicking on overlay");
+                    start(); // Resume tests
+                }, 500);
 
-			});
-			
-		} else {
-			equal(pikabuTest.$navToggles.is(':visible'), false, 
-				"Nav toggles should be hidden in wide screen mode");
-		}
+            });
+            
+        } else {
+            equal(pikabuTest.$navToggles.is(':visible'), false, 
+                "Nav toggles should be hidden in wide screen mode");
+        }
 
-	});
+    });
 
-	test("Pikabu events", function() {
-		// Confirm events work
-		equal(initMarker, true, "Pikabu initialization event failed");
-	});
+    test('Pikabu events', function() {
+        // Confirm events work
+        equal(initMarker, true, "Pikabu initialization event failed");
+    });
 
 })(jQuery);
